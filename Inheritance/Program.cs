4c0ba5c77c1eb42a6035a2597c70af48f237e81b
sarajadidi <sararivkajadidi@gmail.com>
2023-10-10 09:41:20 -0700

    finished Exercise

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -6,34 +6,35 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
-            // TODO Be sure to follow best practice when creating your classes
 
-            // Create a class Animal
-            // give this class 4 members that all Animals have in common
+            var peacock = new Bird();
+            peacock.WingColor = "purple";
+            peacock.CanFly = false;
+            peacock.WillMigrate = true;
+            peacock.BeakLength = 4;
+
+            var snake = new Reptile();
+            
+            snake.IsColdBlooded = true;
+            snake.IsScaly = true;
+            snake.Habitat = "Jungle";
+            snake.CanRegrowTail = false;
+
+            var myAnimals = new Animal[] { peacock, snake };
+
+            foreach(var animal in myAnimals)
+            {
+                Console.WriteLine($"Alive:{animal.IsAlive}");
+                Console.WriteLine($"Age:{animal.Age} years old");
+                Console.WriteLine($"It has:{animal.LegCount} legs");
+                Console.WriteLine($"It lives on {animal.LandSeaAir}");
+                Console.WriteLine($"");
 
 
-            // Create a class Bird
-            // give this class 4 members that are specific to Bird
-            // Set this class to inherit from your Animal Class
-
-            // Create a class Reptile
-            // give this class 4 members that are specific to Reptile
-            // Set this class to inherit from your Animal Class
+            }
 
 
 
-
-            /*Create an object of your Bird class
-             *  give values to your members using the object of your Bird class
-             *  
-             * Creatively display the class member values 
-             */
-
-            /*Create an object of your Reptile class
-             *  give values to your members using the object of your Reptile class
-             *  
-             * Creatively display the class member values 
-             */
         }
     }
 }
---
+++ Inheritance/Reptile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Inheritance
+{
+	public class Reptile : Animal
+	{
+		public Reptile()
+		{
+            IsAlive = true;
+			LegCount = 0;
+            LandSeaAir = "Land";
+            Age = 0;
+
+        }
+
+		public bool IsColdBlooded { get; set; }
+		public bool IsScaly { get; set; }
+		public string Habitat { get; set; }
+		public bool CanRegrowTail { get; set; }
+
+
+
+	}
+}
+
